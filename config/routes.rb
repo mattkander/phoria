59a845c13cf229ebf9f41180e4eeae232cf8a412
mattKjer <mknander6@gmail.com>
2020-06-23 14:44:27 +1200

    Added orders page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'landing_page/index'
 
+  resources :orders
+
   root 'landing_page#index' 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
